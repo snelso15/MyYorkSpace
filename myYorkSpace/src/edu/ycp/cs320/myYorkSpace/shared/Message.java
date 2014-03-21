a09dd5a343a35e803359f220943fedb41192e8e9
abogart1 <abogart1@ycp.edu>
2014-03-21 13:49:30 -0400

    Work done to Message Class

--- myYorkSpace/src/edu/ycp/cs320/myYorkSpace/shared/Message.java
+++ myYorkSpace/src/edu/ycp/cs320/myYorkSpace/shared/Message.java
@@ -1,5 +1,45 @@
 package edu.ycp.cs320.myYorkSpace.shared;
 
 public class Message {
-
+	
+	private String fromUser;
+	private String toUser;
+	private String messText;
+	
+	public Message(String fromUser, String toUser, String messText)
+	{
+		this.fromUser = fromUser;
+		this.toUser = toUser;
+		this.messText = messText;
+	}
+	
+	public void setFromUser(String fromUser)
+	{
+		this.fromUser = fromUser;
+	}
+	
+	public String getFromUser()
+	{
+		return fromUser;
+	}
+	
+	public void setToUser(String toUser)
+	{
+		this.toUser = toUser;
+	}
+	
+	public String getToUser()
+	{
+		return toUser;
+	}
+	
+	public void setMessText(String messText)
+	{
+		this.messText = messText;
+	}
+	
+	public String getMessText()
+	{
+		return messText;
+	}
 }
