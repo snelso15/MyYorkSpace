ecacd00fd04952f4610d4dc743ad325cc4fd78ef
abogart1 <abogart1@ycp.edu>
2014-03-24 13:36:00 -0400

    updated event class

--- myYorkSpace/src/edu/ycp/cs320/myYorkSpace/shared/Event.java
+++ myYorkSpace/src/edu/ycp/cs320/myYorkSpace/shared/Event.java
@@ -1,5 +1,17 @@
 package edu.ycp.cs320.myYorkSpace.shared;
 
+import java.util.ArrayList;
+
 public class Event {
+	
+	private String eventName;
+	private String eventDesc;
+	private String eventTime;
+	private ArrayList<Account> invited;
+	
+	public Event(String eventName, String eventDesc, String eventTime, ArrayList<Account> invited)
+	{
+		
+	}
 
 }
