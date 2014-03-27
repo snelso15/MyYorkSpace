06fbbbdaa2b2fdac37d94cddbea315d62066f07f
abogart1 <abogart1@ycp.edu>
2014-03-27 18:45:20 -0400

    Added code to the Post Class and created a JUnit test for the Post Class

---
+++ myYorkSpace/junit/edu/ycp/cs320/myYorkSpace/shared/PostTest.java
@@ -0,0 +1,37 @@
+package edu.ycp.cs320.myYorkSpace.shared;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import junit.framework.TestCase;
+
+public class PostTest extends TestCase{
+
+	private Post a;
+	private Post b;
+	private Post c;
+	
+	@Before
+	protected void setUp()
+	{
+		a = new Post("Taylor", "Going to the mall, who wants to come?!");
+		b = new Post("Bob", "Just got my truck back from the shop!");
+		c = new Post("Chris", "Thinking about going on a road trip, anyone else want to go?");
+	}
+	
+	@Test
+	public void testGetPostUser() {
+		assertEquals("Taylor", a.getPostUser());
+		assertEquals("Bob", b.getPostUser());
+		assertEquals("Chris", c.getPostUser());
+	}
+	
+	@Test
+	public void testGetPostText() {
+		assertEquals("Going to the mall, who wants to come?!", a.getPostText());
+		assertEquals("Just got my truck back from the shop!", b.getPostText());
+		assertEquals("Thinking about going on a road trip, anyone else want to go?", c.getPostText());
+	}
+}
--- myYorkSpace/src/edu/ycp/cs320/myYorkSpace/shared/Post.java
+++ myYorkSpace/src/edu/ycp/cs320/myYorkSpace/shared/Post.java
@@ -2,4 +2,32 @@ package edu.ycp.cs320.myYorkSpace.shared;
 
 public class Post {
 
+	private String postUser;
+	private String postText;
+	
+	public Post(String postUser, String postText)
+	{
+		this.postUser = postUser;
+		this.postText = postText;
+	}
+	
+	public void setPostUser(String postUser)
+	{
+		this.postUser = postUser;
+	}
+	
+	public String getPostUser()
+	{
+		return this.postUser;
+	}
+	
+	public void setPostText(String postText)
+	{
+		this.postText = postText;
+	}
+	
+	public String getPostText()
+	{
+		return this.postText;
+	}
 }
