31a9dd6a8d43b766d398e2182158d7942f94fbfb
abogart1 <abogart1@ycp.edu>
2014-03-27 15:14:35 -0400

    more changes to the event class

--- myYorkSpace/src/edu/ycp/cs320/myYorkSpace/shared/Event.java
+++ myYorkSpace/src/edu/ycp/cs320/myYorkSpace/shared/Event.java
@@ -11,7 +11,49 @@ public class Event {
 	
 	public Event(String eventName, String eventDesc, String eventTime, ArrayList<Account> invited)
 	{
-		
+		this.eventName = eventName;
+		this.eventDesc = eventDesc;
+		this.eventTime = eventTime;
+		this.invited = invited;
 	}
-
+	
+	public void setEventName(String eventName)
+	{
+		this.eventName = eventName;
+	}
+	
+	public String getEventName()
+	{
+		return this.eventName;
+	}
+	
+	public void setEventDesc(String eventDesc)
+	{
+		this.eventDesc = eventDesc;
+	}
+	
+	public String getEventDesc()
+	{
+		return this.eventDesc;
+	}
+	
+	public void setEventTime(String eventTime)
+	{
+		this.eventTime = eventTime;
+	}
+	
+	public String getEventTime()
+	{
+		return this.eventTime;
+	}
+	
+	public void setInvited(ArrayList<Account> invited)
+	{
+		this.invited = invited;
+	}
+	
+	public ArrayList<Account> getInvited()
+	{
+		return this.invited;
+	}
 }
