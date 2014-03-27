4f16902fed82201ba3bc4b6b13f23b97a89bda42
abogart1 <abogart1@ycp.edu>
2014-03-27 18:17:57 -0400

    Create JUnit test for the Message Class

--- myYorkSpace/junit/edu/ycp/cs320/myYorkSpace/shared/MessageTest.java
+++ myYorkSpace/junit/edu/ycp/cs320/myYorkSpace/shared/MessageTest.java
@@ -3,12 +3,44 @@ package edu.ycp.cs320.myYorkSpace.shared;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.Before;
 
-public class MessageTest {
+import junit.framework.TestCase;
 
+
+public class MessageTest extends TestCase{
+
+	private Message a;
+	private Message b;
+	private Message c;
+	
+	@Before
+	protected void setUp()
+	{
+		a = new Message("Bob", "Chris", "Hey Chris, you busy tonight?");
+		b = new Message("Chris", "Bob", "Not really. Why? Whats up?");
+		c = new Message("Bob", "Chris", "A bunch of us were going to go fishing, thought you might want to come");
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetFromUser() {
+		assertEquals("Bob", a.getFromUser());
+		assertEquals("Chris", b.getFromUser());
+		assertEquals("Bob", c.getFromUser());
+	}
+	
+	@Test
+	public void testGetToUser() {
+		assertEquals("Chris", a.getToUser());
+		assertEquals("Bob", b.getToUser());
+		assertEquals("Chris", c.getToUser());
+	}
+	
+	@Test
+	public void testGetMessText() {
+		assertEquals("Hey Chris, you busy tonight?", a.getMessText());
+		assertEquals("Not really. Why? Whats up?", b.getMessText());
+		assertEquals("A bunch of us were going to go fishing, thought you might want to come", c.getMessText());
 	}
 
 }
