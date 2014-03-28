64b0517ba20de4cfce078760af3cdd59bc316502
abogart1 <abogart1@ycp.edu>
2014-03-28 13:21:42 -0400

    simple stuff

---
+++ myYorkSpace/junit/edu/ycp/cs320/myYorkSpace/shared/AttachmentTest.java
@@ -0,0 +1,25 @@
+package edu.ycp.cs320.myYorkSpace.shared;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import junit.framework.TestCase;
+
+public class AttachmentTest {
+
+	@Before
+	protected void setUp()
+	{
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- myYorkSpace/src/edu/ycp/cs320/myYorkSpace/shared/Attachment.java
+++ myYorkSpace/src/edu/ycp/cs320/myYorkSpace/shared/Attachment.java
@@ -1,5 +1,23 @@
 package edu.ycp.cs320.myYorkSpace.shared;
 
+import java.io.File;
+
 public class Attachment {
 
+	private File attach;
+	
+	public Attachment(File attach)
+	{
+		this.attach = attach;
+	}
+	
+	public void setAttachFile(File attach)
+	{
+		this.attach = attach;
+	}
+	
+	public File getAttachFile()
+	{
+		return attach;
+	}
 }
