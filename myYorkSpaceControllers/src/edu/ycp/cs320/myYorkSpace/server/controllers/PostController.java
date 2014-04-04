bc61d7b7daf9cfeb821c5abb1a1f878dbad5f2a2
abogart1 <abogart1@ycp.edu>
2014-04-04 18:13:31 -0400

    Added methods to PostController

--- myYorkSpaceControllers/src/edu/ycp/cs320/myYorkSpace/server/controllers/PostController.java
+++ myYorkSpaceControllers/src/edu/ycp/cs320/myYorkSpace/server/controllers/PostController.java
@@ -1,5 +1,19 @@
 package edu.ycp.cs320.myYorkSpace.server.controllers;
 
+import java.util.ArrayList;
+
+import edu.ycp.cs320.myYorkSpace.shared.Attachment;
+import edu.ycp.cs320.myYorkSpace.shared.Post;
+
 public class PostController {
-	//implement post controller
+	public void createPost(String postUser, String postText, Attachment postAttach)
+	{
+		
+	}
+	
+	public ArrayList<Post> getPosts(String postUser)
+	{
+		//TODO: implement method
+		return null;
+	}
 }
