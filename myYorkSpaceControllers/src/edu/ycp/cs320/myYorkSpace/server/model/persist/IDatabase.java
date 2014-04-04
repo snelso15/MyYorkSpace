c2a2d51853398b390aeffeaa426a4fa56240234c
Sam Nelson <snelso15@ycp.edu>
2014-04-04 13:38:06 -0400

    Ready to implement basic login UI

--- myYorkSpaceControllers/src/edu/ycp/cs320/myYorkSpace/server/model/persist/IDatabase.java
+++ myYorkSpaceControllers/src/edu/ycp/cs320/myYorkSpace/server/model/persist/IDatabase.java
@@ -4,5 +4,8 @@ import edu.ycp.cs320.myYorkSpace.shared.Account;
 
 public interface IDatabase {
 	public Account logIn(String email, String password);
+	public void addUser(Account userToAdd);
+	public Account findUserByUserName(String name);
+	public Account findUserByEmail(String email);
 }
 
--- myYorkSpacePersistence/src/edu/ycp/cs320/myYorkSpace/server/model/persist/FakeDatabase.java
+++ myYorkSpacePersistence/src/edu/ycp/cs320/myYorkSpace/server/model/persist/FakeDatabase.java
@@ -3,6 +3,7 @@ package edu.ycp.cs320.myYorkSpace.server.model.persist;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.ycp.cs320.booksdb.model.Author;
 import edu.ycp.cs320.myYorkSpace.shared.Account;
 import edu.ycp.cs320.myYorkSpace.shared.Birthday;
 
@@ -38,17 +39,62 @@ public class FakeDatabase implements IDatabase {
 		account.setUserName("Prof");
 		
 		accountList.add(account);
-	
 	}
-
-	@Override
+	public void addUser(Account userToAdd){
+		if(verifyUser(userToAdd)){
+			accountList.add(userToAdd);
+		}
+	}
+	
 	public Account logIn(String email, String password) {
+		Account thisUser = findUserByEmail(email);
+		if(thisUser==null){
+			System.out.println("Account with this Email does not exist");
+			return null;
+		}
+		if (thisUser.getPassword().equals(password)) {
+			return thisUser;
+		}
+		return null;
+	}
+	
+	public Account findUserByUserName(String name) {
 		for (Account acct : accountList) {
-			if (acct.getEmail().equals(email) && acct.getPassword().equals(password)) {
+			if (acct.getUserName().equals(name)) {
 				return acct;
 			}
 		}
 		return null;
+	}
+	
+	public Account findUserByEmail(String email) {
+		for (Account acct : accountList) {
+			if (acct.getEmail().equals(email)) {
+				return acct;
+			}
+		}
+		return null;
+	}
+	
+	public boolean verifyUser(Account user){
+		if(validEmail(user.getEmail())&&validPassword(user.getPassword())) return true;
+		else return false;
+	}
+	
+	public boolean validEmail(String email){
+		boolean valid = false;
+		if(email.contains("@")&&(email.contains(".edu"))) valid=true;
+		return valid;
+	}
+	public boolean validPassword(String password){
+		//password must be at least 5 characters long
+		if(password.length()<5)return false;
+		//password must be sequence of numbers between 0-9 or capitol/lower case letters.
+		boolean valid = true;
+		for(int i = 0; i<password.length()-1; i++){
+			if(!(password.charAt(i) >= '0' && password.charAt(i) <= '9')&&!(password.charAt(i) >= 'a' && password.charAt(i) <= 'z')&& !(password.charAt(i) >= 'A' && password.charAt(i) <= 'Z')) valid=false;
+		}
+		return valid; 
 	}
 
 }
