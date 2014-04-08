b4c17d49cd8ab5af86cf8e6574cfb6c6ff685e54
abogart1 <abogart1@ycp.edu>
2014-04-07 21:25:33 -0400

    I think I fixed it... Hopefully

--- myYorkSpaceControllers/src/edu/ycp/cs320/myYorkSpace/server/controllers/PostController.java
+++ myYorkSpaceControllers/src/edu/ycp/cs320/myYorkSpace/server/controllers/PostController.java
@@ -8,7 +8,7 @@ import edu.ycp.cs320.myYorkSpace.shared.Post;
 public class PostController {
 	public void createPost(String postUser, String postText, Attachment postAttach)
 	{
-		
+		//TODO: implement method
 	}
 	
 	public ArrayList<Post> getPosts(String postUser)
