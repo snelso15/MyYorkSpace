1ee6286d63c3965e449650c9a43cd3f6e3ff11c5
abogart1 <abogart1@ycp.edu>
2014-04-11 13:49:55 -0400

    Post controller and message controller

--- myYorkSpaceControllers/src/edu/ycp/cs320/myYorkSpace/server/controllers/PostController.java
+++ myYorkSpaceControllers/src/edu/ycp/cs320/myYorkSpace/server/controllers/PostController.java
@@ -2,18 +2,19 @@ package edu.ycp.cs320.myYorkSpace.server.controllers;
 
 import java.util.ArrayList;
 
+import edu.ycp.cs320.myYorkSpace.server.model.persist.DatabaseProvider;
 import edu.ycp.cs320.myYorkSpace.shared.Attachment;
 import edu.ycp.cs320.myYorkSpace.shared.Post;
 
 public class PostController {
 	public void createPost(String postUser, String postText, Attachment postAttach)
 	{
-		//TODO: implement method
+		Post p = new Post(postUser, postText, postAttach);
+		DatabaseProvider.getInstance().createPost(p);
 	}
 	
-	public ArrayList<Post> getPosts(String postUser)
+	public ArrayList<Post> getPost(String postUser)
 	{
-		//TODO: implement method
-		return null;
+		return DatabaseProvider.getInstance().getPosts(postUser);
 	}
 }
