a6076acaf240b4a8f285b95420510366642e445e
abogart1 <abogart1@ycp.edu>
2014-04-23 13:04:40 -0400

    jasdlfajdfl;

---
+++ myYorkSpaceWebApp/src/edu/ycp/cs320/myYorkSpace/client/ProfileService.java
@@ -0,0 +1,10 @@
+package edu.ycp.cs320.myYorkSpace.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("profile")
+public interface ProfileService extends RemoteService{
+
+}
---
+++ myYorkSpaceWebApp/src/edu/ycp/cs320/myYorkSpace/client/ProfileServiceAsync.java
@@ -0,0 +1,7 @@
+package edu.ycp.cs320.myYorkSpace.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface ProfileServiceAsync {
+
+}
